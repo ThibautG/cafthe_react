6401ed961a92e222576df9c5a83df0ed26adb26e
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-10 09:38:16 +0100

    Initialisation projet

---
+++ src/layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+/* npm install react-router-dom */
+/* Le but de Layout est de créer un template où on va mettre les éléments qui s'affichent tout le temps*/
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Layout(props) {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            {/* Outlet : là où s'affichent les pages enfants*/}
+            <Outlet />
+            <Footer />
+        </div>
+    );
+}
+
+export default Layout;
