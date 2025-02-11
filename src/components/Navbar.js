79ce57d03726309d4319e020eeef37e663aab9c3
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-11 10:58:35 +0100

    Création fichier AuthContext.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar(props) {
     return (
         <div>
-            <ul>
+            <ul className={"navbar"}>
                 <li><Link to={`/`}>Accueil</Link></li>
                 <li><Link to={`/produits/cafes`}>Cafés</Link></li>
                 <li><Link to={`/produits/thes`}>Thés</Link></li>
---
+++ src/context/AuthContext.js
@@ -0,0 +1,12 @@
+import React, {createContext, useState, useEffect} from 'react';
+
+/* Exportation du contexte pour y avoir accès */
+export const AuthContext = createContext(null);
+
+function AuthContext(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default AuthContext;
