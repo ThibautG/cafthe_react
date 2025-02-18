6d2163afd2bd4227bd437f560cfec4ea483e2f9e
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-18 16:50:25 +0100

    Création détails commandes

--- src/components/CommandDetail.js
+++ src/components/CommandDetail.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import "../styles/CommandDetail.css"
+import {useParams} from "react-router-dom";
+import axios from "axios";
 
 function CommandDetail(props) {
+    const {id} = useParams();
+
+    const [detail, setDetail] = useState([]);
+
+    useEffect(() => {
+        const fetchDetail = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/api/commandes/${id}`);
+                setDetail(response.data);
+            } catch (error) {
+                console.error("Erreur de chargement de la commande", error);
+            }
+        };
+
+        void fetchDetail();
+    }, [id]);
+
+    console.log(detail)
+
     return (
-        <div></div>
+        <div>Coucou les détails</div>
     );
 }
 
