3449b187249e7415e1e0fec25750fccdc6930625
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-19 09:07:47 +0100

    affichage des détails commande avec map + gestion des cas ou les commandes sont vides ou inexistantes

--- src/components/CommandDetail.js
+++ src/components/CommandDetail.js
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from 'react';
 import "../styles/CommandDetail.css"
 import {Link, useParams} from "react-router-dom";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
 
 function CommandDetail(props) {
     const {id} = useParams();
 
     const [detail, setDetail] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchDetail = async () => {
@@ -16,13 +18,42 @@ function CommandDetail(props) {
                 setDetail(response.data);
             } catch (error) {
                 console.error("Erreur de chargement de la commande", error);
+            } finally {
+                setIsLoading(false); /* on arrête d'afficher les éléments de chargement (squelette)*/
             }
         };
 
         void fetchDetail();
     }, [id]);
 
-    /*console.log(detail)*/
+
+    console.log(detail)
+
+    if (isLoading) {
+        return (
+            <div className={"product-list"}>
+                {Array.from({length: detail.length}).map((_, i) => (
+                    <div key={i} className={"product-skeleton"}>
+                        {/* image */}
+                        <Skeleton height={200} width={300}/>
+
+                        <div style={{marginTop: "10px"}}>
+                            <Skeleton height={20} width={"70%"}/>
+                        </div>
+
+                        <div style={{marginTop: "10px"}}>
+                            <Skeleton height={20} width={"40%"}/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    if (!detail || detail.length === 0) {
+        return <p>La commande est vide ou inexistante.</p>;
+    }
+
 
     return (
         <div>
@@ -44,6 +75,10 @@ function CommandDetail(props) {
                     <p>Statut de la commande : {detail[0].Statut_commande}</p>
                     <p>Type de commande : {detail[0].Type_commande}</p>
                 </div>
+
+                {/*<Link to={`/commandes/clients/${user.id}`} className={"details-btn"}>
+                    retour aux commandes
+                </Link>*/}
             </div>
         </div>
     );
