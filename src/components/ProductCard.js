f1902fa9974869d6f96b82eed9cabb3ed2a84414
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-25 11:10:50 +0100

    Ajout d'un bouton pour addToCart sur onClick

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
 import "../styles/ProductCard.css"
+import {CartContext} from "../context/CartContext";
 
 function ProductCard({produit}) {
+    const { addToCart } = useContext(CartContext);
+
     return (
         <div className={"product-card"}>
             {/* image */}
             <h3>{produit.Designation_produit}</h3>
             <p>{produit.Prix_ttc_produit}</p>
+            <button onClick={() => addToCart(produit) }>Ajouter au panier</button>
             <Link to={`/produit/${produit.Identifiant_produit}`} className={"details-btn"}>
                 Voir détails
             </Link>
