6cb0c9b4ea9ed140cace3d6fed0c1e841ffc2a84
ThibautG <thibaut.gaudinat@gmail.com>
2025-02-25 07:49:26 +0100

    State cart + récupérer cart de localstorage + charger carte dans localstorage à chaque modification + fonction addToCart

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,24 +3,42 @@ import React, {createContext, useState, useEffect} from 'react';
 /* Exportation du contexte pour y avoir accès */
 export const CartContext = createContext(null);
 
-/* Création du provider pour la connexion/déconnexion */
+/* Création du provider pour le panier */
 export function CartProvider({ children }) {
-    const storedCart = localStorage.getItem("cart");
-    const [cart, setCart] = useState(storedCart);
+    // récupération du panier dans le local storage
+    const savedCart = localStorage.getItem('cart');
+    // déclaration du state pour mettre panier à jour (panier vide si rien dans local storage)
+    const [cart, setCart] = useState(savedCart ? JSON.parse(savedCart) : [])
 
-    // Connexion (on reçoit les données envoyées par l'API : le token + les infos client)
-    const addToCart = (idProduit) => {
-        storeCart()
+    // mise à jour du panier dans localstorage à chaque modification dans panier
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart]);
+
+    // création fonction ajouter au panier
+    const addToCart = (produit) => {
+        const produitSelectionne = cart.find((cafthe) => cafthe.name === produit.name)
+        if (produitSelectionne) {
+            const cartFiltreProduitSelectionne = cart.filter(
+                (cafthe) => cafthe.name !== produit.name
+            )
+            setCart([
+                ...cartFiltreProduitSelectionne,
+                { ...produit, qtt: produitSelectionne.qtt + 1 }
+            ])
+        } else {
+            setCart([...cart, { ...produit, amount: 1 }])
+        }
     }
 
-    // Déconnexion
-    const removeFromCart = () => {
-        l);
-    };
+    // fonction retirer du panier
+    /*const removeFromCart = () => {
+
+    };*/
 
     const value = {
         addToCart,
-        removeFromCart,
+
     }
 
 
