30496edbd393cdca0c562398568fc9e804029b5b
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-07 22:23:23 +0200

    ajout du picto search + modif navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,6 +20,7 @@ function Navbar(props) {
                 <li className={"navbar-links"}><Link to={`/produits/cafes`}>Cafés</Link></li>
                 <li className={"navbar-links"}><Link to={`/produits/thes`}>Thés</Link></li>
                 <li className={"navbar-links"}><Link to={`/produits/accessoires`}>Accessoires</Link></li>
+                <li className={"navbar-search-icon nav-icons"}></li>
 
                 <li className={"navbar-button"}>
                     {isAuthenticated ? (
