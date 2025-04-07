cfb8095d002982b5c56ac4d2f745a5359bcc7b6b
ThibautG <thibaut.gaudinat@gmail.com>
2025-04-07 03:16:45 +0200

    commands + profil OK

--- src/pages/Commands.js
+++ src/pages/Commands.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
-import ProductCard from "../components/ProductCard";
 import "../styles/Commands.css"
 
-function Commands(props) {
+function Commands({id}) {
+/*
     const user = JSON.parse(localStorage.getItem('user')); // on récupère les infos user dans localStorage
+*/
 
     const [commands, setCommands] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -14,7 +15,7 @@ function Commands(props) {
         const fetchProduits = async () => {
             try {
                 // route get de toutes les commandes d'un client
-                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/commandes/clients/${user.id}`);
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/commandes/clients/${id}`);
                 setCommands(response.data);
             } catch (error) {
                 console.error("Erreur de chargement des produits", error);
@@ -24,51 +25,65 @@ function Commands(props) {
         };
 
         void fetchProduits();
-    }, []);
-    console.log(commands !== [])
-    console.log(commands)
+    }, [id]);
+    /*console.log(commands !== [])
+    console.log(commands)*/
+
+    if (isLoading) {
+        return (
+            <div className="section-commande">
+                <p>Chargement de vos commandes...</p>
+            </div>
+        );
+    }
+
 
     if (!commands || commands.length === 0) {
         return (
-            <div>
-                <p>Oups...Il semblerait qua vous n'avez encore passé aucune commande chez nous.</p>
-                <Link to={`/profil`} className={"details-btn"}>
-                    Voir mon profil
-                </Link>
-
-                <Link to={`/`} className={"details-btn"}>
-                    Retour à l'accueil
+            <div className={"section-commande"}>
+                <p>Vous n'avez encore passé aucune commande chez nous.
+                    Mais il n’est jamais trop tard pour découvrir nos produits !</p>
+                <Link to={`/`} className="global-link">
+                    Explorer la boutique
                 </Link>
             </div>
         )
     }
 
     return (
-            <section>
-                <h3>Vos commandes</h3>
-                <div className={"command-list"}>
+            <div className={"section-command"}>
+                <h2 className={"section-command-title"}>Vos commandes</h2>
+                <p className={"section-command-subtitle"}>Nombre total de commandes : {commands.length}</p>
+                <div className={"command-list"} >
                     {commands.map((command) => (
-                        <div className={"command"}>
-                            <p>Identifiant commande : {command.Identifiant_commande}</p>
-                            <p>Date commande : {(command.Date_commande).split('T')[0]}</p>
-                            <p>Montant TTC commande : {command.Montant_ttc_commande}€</p>
-                            <p>Type commande : {command.Type_commande}</p>
-                            <p>Statut commande : {command.Statut_commande}</p>
-                            <Link to={`/commandes/${command.Identifiant_commande}`} className={"details-btn"}>
-                                Voir détails
-                            </Link>
+                        <div className={"command-card"} key={command.Identifiant_commande}>
+                            <div className={"command-card-id"}>
+                                <p><strong>Commande #{command.Identifiant_commande}</strong></p>
+                                <p>
+                                    <Link to={`/commandes/${command.Identifiant_commande}`}
+                                          className={"global-link command-card-link"}>
+                                        Voir détails
+                                    </Link>
+                                </p>
+                            </div>
+                            <div className={"command-card-date"}>
+                                <p><strong>Date
+                                    :</strong> {command.Date_commande ? command.Date_commande.split('T')[0] : 'Date inconnue'}
+                                </p>
+                                <p><strong>Statut :</strong> {command.Statut_commande}</p>
+                            </div>
+                            <div className={"command-card-price"}>
+                                <p><strong>Montant :</strong> {command.Montant_ttc_commande}€</p>
+                                <p><strong>Type :</strong> {command.Type_commande}</p>
+                            </div>
                         </div>)
                     )}
                 </div>
 
-                <Link to={`/profil`} className={"details-btn"}>
-                    Voir mon profil
-                </Link>
-
-                <Link to={`/`} className={"details-btn"}>
+                <Link to={`/`} className={"global-btn-secondary"}>
                     Retour à l'accueil
                 </Link>
-        </section>
+        </div>
 
     );
 }
